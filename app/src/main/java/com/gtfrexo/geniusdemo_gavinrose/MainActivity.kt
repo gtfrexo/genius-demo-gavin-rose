0678e97676e71dcd031f46e1ef1583ae0a50c9c7
gtfrexo <gtfrexo@gmail.com>
2018-04-30 15:20:28 -0500

    Endpoint and connection created.

--- app/src/main/java/com/gtfrexo/geniusdemo_gavinrose/MainActivity.kt
+++ app/src/main/java/com/gtfrexo/geniusdemo_gavinrose/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.gtfrexo.geniusdemo_gavinrose
 
+import android.os.AsyncTask
+import android.os.AsyncTask.execute
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import java.net.URL
+import java.net.URLConnection
+import javax.net.ssl.HttpsURLConnection
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Create endpoint reference and open connection.
+        val reqresEndpoint = URL("https://reqres.in/api/users")
+        val reqresConnection = reqresEndpoint.openConnection() as HttpsURLConnection
+        reqresConnection.setRequestProperty("User-Agent", "genius-demo-gavin-rose-v0.1")
+
+        //Create background thread here.
+        //Usually, I would use RxJava or Coroutines for Asynchronous tasks.
+        AsyncTask.execute {
+
+        }
     }
 }
